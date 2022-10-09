0a717299b9f97ab641db343b2a0a7763b3f49dde
Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
2022-10-09 14:56:33 -0500

    Create oop.dart

---
+++ oop.dart
@@ -0,0 +1,22 @@
+main() {
+ Person clark = Person();
+ clark.name = 'Clark';
+ clark.surname = 'Kent';
+ clark.age = 30;
+ print ('${clark.name} ${clark.surname} ${clark.age}');
+}
+class Person {
+ String name, surname;
+ int _age;
+ void set age(int years) {
+ if (years > 0 && years < 120) {
+ _age = years;
+ }
+ else {
+ _age = 0;
+ }
+ }
+ int get age {
+ return _age;
+ }
+}
